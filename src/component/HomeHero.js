4cacb16da8646b14c1b3191322ade66605e2a4a7
Abdulrahmanololade <abdulrahmanololade15@gmail.com>
2024-07-16 11:40:55 +0100

    first commit

---
+++ src/component/HomeHero.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function HomeHero(){
+    return(
+        <div className="hero">
+            <div className="overlay"></div>
+            <div className="content">
+                <h4>Best Hotel With Cozy Rooms</h4>
+                <h1>Just Experience a <br></br> New Level Of <br></br>Luxury.</h1>
+
+            </div>
+        </div>
+    )
+}
+
+export default HomeHero
---
+++ src/component/navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+function NavBar (){
+    return(
+        <ul className="navbar">
+            <div className="logo">
+            </div>
+            <div className="nav">
+            <li><a href="www,google.com">Home</a></li>
+            <li><a href="www.google.com">About us</a></li>
+            <li><a href="www.google.com">Rooms</a></li>
+            <li><a href="www.google.com">BookARoom</a></li>
+            <li><a href="www.google.com">Contact</a></li>
+            </div>
+        </ul>
+    )
+}
+
+export default NavBar;
